f39cc9cd44418af5327f56850b7b4c3f869d62fb
Satyam-Bhatt <satyambhatt20@gmail.com>
2024-03-23 18:49:57 +0530

    Basic Structure set

---
+++ Assets/GameManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown && !Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Debug.Log(Input.inputString);
+        }
+
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Ray ray = new Ray(mousePosition,Vector3.forward);
+
+        RaycastHit2D hitInfo = Physics2D.Raycast(ray.origin, ray.direction);
+        if(hitInfo)
+        {
+
+        }
+    }
+}
