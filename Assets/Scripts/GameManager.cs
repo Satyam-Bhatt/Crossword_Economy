a637799de67d174d1dfd5c2c3f09c9dd19774068
Satyam-Bhatt <satyambhatt20@gmail.com>
2024-03-24 19:21:52 +0530

    Letter transportation polished

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,9 +7,20 @@ public class GameManager : MonoBehaviour
 {
     private string letter_Hold;
 
+    [SerializeField] private GameObject letterShow;
+    private GameObject letterShow_Container;
+    private Canvas canvas;
+
+    private void Start()
+    {
+        canvas = GameObject.Find("Canvas").GetComponent<Canvas>();
+    }
+
     // Update is called once per frame
     void Update()
     {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
         if (Input.anyKeyDown && !Input.GetKeyDown(KeyCode.Mouse0))
         {
             Debug.Log(Input.inputString);
@@ -17,7 +28,7 @@ public class GameManager : MonoBehaviour
 
         if (Input.GetMouseButtonDown(0))
         {
-            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            
             Ray ray = new Ray(mousePosition,Vector3.forward);
 
             RaycastHit2D hitInfo = Physics2D.Raycast(ray.origin, ray.direction);
@@ -29,15 +40,22 @@ public class GameManager : MonoBehaviour
                     if (text.text != "")
                     {
                         letter_Hold = hitInfo.transform.gameObject.GetComponentInChildren<TMP_Text>().text;
+                        letterShow_Container = Instantiate(letterShow, new Vector2(mousePosition.x, mousePosition.y), Quaternion.identity);
+                        letterShow_Container.transform.SetParent(canvas.transform, false);
+                        letterShow_Container.GetComponent<TMP_Text>().text = letter_Hold;
                     }
                 }
                 
             }
         }
 
+        if(letterShow_Container != null)
+        {
+            letterShow_Container.transform.position = new Vector2(mousePosition.x, mousePosition.y);
+        }
+
         if(Input.GetKeyUp(KeyCode.Mouse0))
         {
-            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             Ray ray = new Ray(mousePosition, Vector3.forward);
 
             RaycastHit2D hitInfo = Physics2D.Raycast(ray.origin, ray.direction);
@@ -53,7 +71,7 @@ public class GameManager : MonoBehaviour
                 }
 
             }
-
+            Destroy(letterShow_Container) ;
             letter_Hold = null;
         }
 
